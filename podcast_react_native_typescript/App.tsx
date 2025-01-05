da8073f32cb1c664b80ff16f01615563719ac086
FabioCunha <fabiobh@hotmail.com.br>
2025-01-05 15:36:16 -0300

    define inital setup

--- podcast_react_native_typescript/App.tsx
+++ podcast_react_native_typescript/App.tsx
@@ -1,10 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Audio } from 'expo-av';
+import { useState, useEffect } from 'react';
+
+var mp3file = "https://musopen.org/static/music/performances/2013/09/Clair_de_Lune.mp3"
 
 export default function App() {
+  const [sound, setSound] = useState<Audio.Sound | null>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    async function loadSound() {
+      const { sound } = await Audio.Sound.createAsync({ uri: mp3file });
+      setSound(sound);
+    }
+    loadSound();
+    return () => {
+      if (sound) {
+        sound.unloadAsync();
+      }
+    };
+  }, []);
+
+  const playSound = async () => {
+    if (sound) {
+      if (isPlaying) {
+        await sound.pauseAsync();
+      } else {
+        await sound.playAsync();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title={isPlaying ? "Pause" : "Play"} onPress={playSound} />
       <StatusBar style="auto" />
     </View>
   );
